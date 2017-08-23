4debc7edb4aad258dee9cd0221650f9bbb0b441b
minkto <p_achikeh@yahoo.co.uk>
2017-08-23 21:08:19 +0100

    Added some comments to the script to ensure greater clarity.

--- scripts/cardJs.js
+++ scripts/cardJs.js
@@ -1,3 +1,4 @@
+/* Global variables which contain references to each input option via their names.*/
 var titleInputBox = document.getElementsByName('cardtitle')[0];
 var forenameInputBox = document.getElementsByName('forename')[0];
 var surnameInputBox = document.getElementsByName('surname')[0];
@@ -8,11 +9,17 @@ var dateInput = document.getElementsByName('date-of-birth')[0]
 var submitButton = document.getElementsByName('enter')[0];
 
 
+/* This method helps to change the photo icon based on what is uploaded */
 function changeImage() {
+	// Gets the file selected from the prompt.
 	var file = fileInputButton.files[0];
+	// The regex for ensuring an image is selected.
 	var imageType = 'image.*/'
+	
+	// The card photo element.
 	var myPhoto = document.getElementById('card-photo');
-		
+	
+   // Checks the file type is actually an image.	
 	if(file.type.match(imageType)) {
 	
 	    var fileReader = new FileReader();
@@ -29,13 +36,14 @@ function changeImage() {
 	  
 
 
-
+/*Changes the title of the card */
 function changeCardTitle() {
 	var textName = document.getElementsByTagName('h2')[0];
     textName.innerHTML = titleInputBox.value;
 }
 
 
+/* Change forename displayed on the card*/
 function changeCardForename() {
 	var textName = document.getElementsByClassName('card-label')[0];
 	textName.textContent = "Forename:"
@@ -43,13 +51,14 @@ function changeCardForename() {
 }
 
 
-
+/* Change the surname of the card selected.*/
 function changeCardSurname() {
 	var textName = document.getElementsByClassName('card-label')[1];
 	textName.textContent = "Surname:"
 	textName.textContent += surnameInputBox.value;
 }
 
+/* Change the date that is show on the card*/
 function changeDate() {
 	var textDate = document.getElementsByClassName('card-label')[2];
 	textDate.textContent = "Date Of Birth:";
@@ -57,17 +66,17 @@ function changeDate() {
 	
 }
 
+/* Change the color of the card.*/
 function changeColor() {
 	var cardColor = document.getElementsByClassName('card-container')[0];
 	cardColor.style.backgroundColor = colorButton.value;
 	//cardColor.style.backgroundColor = 'rgb('+255 +',' +255 + ',' + 255 +')'; 	
 }
 
+/* Change the orientation of the card selected.*/
 function changeCardOrientation() {
 	var currentCss = document.getElementsByTagName('link')[0];
-	
-	
-	
+		
 	switch(currentCss.getAttribute('href')) {
 		case 'styles/card-horizontal.css':
 			currentCss.setAttribute('href','styles/card-vertical.css');
@@ -79,6 +88,8 @@ function changeCardOrientation() {
 	
 }
 
+/* This is where all the globalistener events are associatiated with that
+ appropriate function.*/
 forenameInputBox.oninput = changeCardForename;
 surnameInputBox.oninput = changeCardSurname;
 titleInputBox.oninput = changeCardTitle;
