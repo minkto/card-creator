1219f4ad949732292cfe91fb2660d79773058e96
minkto <p_achikeh@yahoo.co.uk>
2017-09-02 17:07:18 +0100

    This version of the card script makes the card an object. Rather then rely on global functions, this one only mainly uses the submit button to update the card.

--- scripts/cardJs.js
+++ scripts/cardJs.js
@@ -1,10 +1,6 @@
 /* Global variables which contain references to each input option via their names.*/
-var titleInputBox = document.getElementsByName('cardtitle')[0];
-var forenameInputBox = document.getElementsByName('forename')[0];
-var surnameInputBox = document.getElementsByName('surname')[0];
-var colorButton = document.getElementsByName('color')[0];
-var fileInputButton = document.getElementsByName('file-name')[0];
-var dateInput = document.getElementsByName('date-of-birth')[0]
+
+
 
 var submitButton = document.getElementsByName('finish')[0];
 
@@ -90,10 +86,82 @@ function changeCardOrientation() {
 
 /* This is where all the globalistener events are associatiated with that
  appropriate function.*/
-forenameInputBox.oninput = changeCardForename;
+
+ var card = {
+	 title:function() {
+        var titleInputBox = document.getElementsByName('cardtitle')[0];
+		var textName = document.getElementsByTagName('h2')[0];
+		var theTitle = titleInputBox.value;
+        textName.innerHTML = theTitle;
+		return theTitle;
+	 },
+	 forename:function() {
+		var forenameInputBox = document.getElementsByName('forename')[0];
+		var textName = document.getElementsByClassName('card-label')[0];
+		textName.textContent = "Forename:";
+		textName.textContent += forenameInputBox.value;
+	 },
+	 surname:function() {
+		var surnameInputBox = document.getElementsByName('surname')[0];
+		var textName = document.getElementsByClassName('card-label')[1];
+		textName.textContent = "Surname:"
+		textName.textContent += surnameInputBox.value;
+		
+	 },
+	 birth:function() {
+		var dateInput = document.getElementsByName('date-of-birth')[0];
+		var textDate = document.getElementsByClassName('card-label')[2];
+		textDate.textContent = "Date Of Birth:";
+		textDate.textContent+= dateInput.value;
+		
+	 },
+	 picture:function() {
+		 var fileInputButton = document.getElementsByName('file-name')[0];
+		 // Gets the file selected from the prompt.
+		var file = fileInputButton.files[0];
+		// The regex for ensuring an image is selected.
+		var imageType = 'image.*/'
+	
+		// The card photo element.
+		var myPhoto = document.getElementById('card-photo');
+	
+		// Checks the file type is actually an image.	
+		if(file.type.match(imageType)) {
+	
+			var fileReader = new FileReader();
+			fileReader.onload = function () {
+				var URLImage = fileReader.result;
+				myPhoto.src = URLImage;
+		
+			}
+		} else {
+			console.log("Wrong format!");
+		}
+		fileReader.readAsDataURL(file);
+	 },
+	 
+	 foreground:function() {
+		var colorButton = document.getElementsByName('color')[0];
+		var cardColor = document.getElementsByClassName('card-container')[0];
+		cardColor.style.backgroundColor = colorButton.value;
+		//cardColor.style.backgroundColor = 'rgb('+255 +',' +255 + ',' + 255 +')'; 	
+	 }
+ 
+ };
+ 
+ submitButton.addEventListener("click", function() {
+	 card.title();
+	 card.forename();
+	 card.surname();
+	 card.birth();
+	 card.picture();
+	 card.foreground();
+	} ,false);
+ 
+ /*forenameInputBox.oninput = changeCardForename;
 surnameInputBox.oninput = changeCardSurname;
 titleInputBox.oninput = changeCardTitle;
 colorButton.onchange = changeColor;
 fileInputButton.onchange = changeImage;
 dateInput.oninput = changeDate;
-
+*/
